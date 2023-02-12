c6c765fe4c1fb00f4a23b7d025784d15cd5d8d99
preetbhullar90 <62619558+preetbhullar90@users.noreply.github.com>
2023-02-12 01:58:04 +0000

    all

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,9 @@ const Header = () => {
     <div className='flex items-center bg-amazon_blue p-1 flex-grow py-2'>
 <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
     <Image 
-        src="https://links.papareact.com/f90"
+        src="/images/Chicken-Curry-recipe.jpg"
+        // amazon-project\public\images\Chicken-Curry-recipe.jpg
+        // https://links.papareact.com/f90
         width={90}
         height={40}
         style={{objectFit:"contain"}}
